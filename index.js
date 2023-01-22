6b79fe9ad0e9a37d7cf3f20a5fed3ecb92282bec
Tushaar <tushaarrao007@gmail.com>
2023-01-23 00:18:47 +0530

    crud and design

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const ejsMate = require('ejs-mate')
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const campground = require('./models/campgrounds');
 
 
@@ -13,21 +15,52 @@ db.once("open",() => {
     console.log("Database Connected");
 });
 
-
+app.engine('ejs',ejsMate);
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'))
 
+app.use(express.urlencoded({ extended:true }));
+app.use(methodOverride('_method'));
+
 app.get('/', (req,res) => {
     res.render("home");
-})
+});
 
 app.get('/campgrounds', async(req,res) => {
     const allcamps = await campground.find({});
     res.render('campgrounds/index', { allcamps })
+});
+
+app.get('/campgrounds/:id/edit', async(req,res) => {
+    const camp = await campground.findById(req.params.id);
+    res.render('campgrounds/edit',{ camp });
+})
+
+app.get('/campgrounds/new', (req,res) => {
+    res.render('campgrounds/new');
 })
 
 app.get('/campgrounds/:id', async(req,res) => {
-    res.render()
+    const camp = await campground.findById(req.params.id);
+    res.render('show',{ camp })
+});
+
+app.post('/campgrounds', async(req,res) => {
+    const camp = new campground(req.body.campground);
+    await camp.save();
+    res.redirect(`/campgrounds/${camp._id}`)
+})
+
+app.put('/campgrounds/:id',async(req,res) => {
+    const { id } = req.params;
+    await campground.findByIdAndUpdate(id,{...req.body.campground});
+    res.redirect(`/campgrounds/${id}`);
+})
+
+app.delete('/campgrounds/:id',async(req,res) => {
+    const { id } = req.params;
+    await campground.findByIdAndDelete(id,{...req.body.campground});
+    res.redirect(`/campgrounds`);
 })
 
 app.listen(3000, () => {
--- seeds/index.js
+++ seeds/index.js
@@ -18,10 +18,14 @@ const seedDB = async() => {
     await campground.deleteMany({});
     for(let i = 0;i<50;i++){
         const rand = Math.floor(Math.random()*1000);
+        const price = Math.floor(Math.random() * 30) + 10;
 
         const camps = new campground({
             location:`${cities[rand].city},${cities[rand].state}`,
-            title: `${sample(descriptors)} , ${sample(places)}`
+            title: `${sample(descriptors)} , ${sample(places)}`,
+            image:`https://source.unsplash.com/random/300x300?camping,${i}`,
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa dignissimos tempore iure qui voluptas cupiditate laudantium! Delectus, officiis id eius architecto velit expedita eaque quidem veniam, inventore itaque soluta atque.',
+            price
         })
 
         await camps.save();
